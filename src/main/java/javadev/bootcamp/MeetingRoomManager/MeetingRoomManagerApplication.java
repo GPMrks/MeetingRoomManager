a4ab0d00f86463c726621134cc468f450d45ad7e
GPMrks <guilhermepereiramarques@hotmail.com>
2022-04-25 15:58:12 -0300

    INIT - Uploading Initial Files

---
+++ src/main/java/javadev/bootcamp/MeetingRoomManager/MeetingRoomManagerApplication.java
@@ -0,0 +1,13 @@
+package javadev.bootcamp.MeetingRoomManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomManagerApplication.class, args);
+	}
+
+}
