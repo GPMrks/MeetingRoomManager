2275ab431fd17143dbe1d82d4cd28c5a20a17326
GPMrks <guilhermepereiramarques@hotmail.com>
2022-04-27 19:21:47 -0300

    FEAT - Added Unit Tests

--- src/test/java/javadev/bootcamp/MeetingRoomManager/MeetingRoomManagerApplicationTests.java
+++ src/test/java/javadev/bootcamp/MeetingRoomManager/MeetingRoomManagerApplicationTests.java
@@ -3,7 +3,6 @@ package javadev.bootcamp.MeetingRoomManager;
 import javadev.bootcamp.MeetingRoomManager.exception.ResourceNotFoundException;
 import javadev.bootcamp.MeetingRoomManager.model.Room;
 import javadev.bootcamp.MeetingRoomManager.service.RoomService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,104 +13,102 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class MeetingRoomManagerApplicationTests {
 
-	@Autowired
-	private RoomService roomService;
+    @Autowired
+    private RoomService roomService;
 
-	@Test
-	public void getRoomTest() {
+    @Test
+    public void getRoomTest() {
 
-		// given - setup or precondition
-		Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
+        // given - setup or precondition
+        Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
 
-		// when - action or the testing
-		List<Room> allRooms = roomService.getAllRooms();
+        // when - action or the testing
+        List<Room> allRooms = roomService.getAllRooms();
 
-		// then - very output
-		assertNotNull(allRooms);
-	}
+        // then - very output
+        assertNotNull(allRooms);
+    }
 
-	@Test
-	public void getRoomByIdTest() throws ResourceNotFoundException {
+    @Test
+    public void getRoomByIdTest() throws ResourceNotFoundException {
 
-		// given - setup or precondition
-		Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
-		Room savedRoom = roomService.createRoom(room);
+        // given - setup or precondition
+        Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
+        Room savedRoom = roomService.createRoom(room);
 
-		// when - action or the testing
-		Room roomById = roomService.getRoomById(savedRoom.getId());
+        // when - action or the testing
+        Room roomById = roomService.getRoomById(savedRoom.getId());
 
-		// then - very output
-		assertEquals(room.toString(), roomById.toString());
-	}
+        // then - very output
+        assertEquals(room.toString(), roomById.toString());
+    }
 
-	@Test
-	public void exceptionTest() throws ResourceNotFoundException {
+    @Test
+    public void exceptionTest() throws ResourceNotFoundException {
 
-		// given - setup or precondition
-		Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
-		Room savedRoom = roomService.createRoom(room);
+        // given - setup or precondition
+        Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
+        Room savedRoom = roomService.createRoom(room);
 
-		Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
-			Room roomById = roomService.getRoomById(2L);
-		});
+        Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
+            Room roomById = roomService.getRoomById(2L);
+        });
 
-		// when - action or the testing
-		String expectedMessage = "Room not found with ID: " + 2L;
-		String actualMessage = exception.getMessage();
+        // when - action or the testing
+        String expectedMessage = "Room not found with ID: " + 2L;
+        String actualMessage = exception.getMessage();
 
-		// then - very output
-		assertTrue(actualMessage.contains(expectedMessage));
-	}
+        // then - very output
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
 
-	@Test
-	public void createRoomTest() {
+    @Test
+    public void createRoomTest() {
 
-		// given - setup or precondition
-		Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
+        // given - setup or precondition
+        Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
 
-		// when - action or the testing
-		Room savedRoom = roomService.createRoom(room);
+        // when - action or the testing
+        Room savedRoom = roomService.createRoom(room);
 
-		// then - very output
-		assertEquals(room.toString(), savedRoom.toString());
-	}
+        // then - very output
+        assertEquals(room.toString(), savedRoom.toString());
+    }
 
-	@Test
-	public void updateRoomTest() throws ResourceNotFoundException {
+    @Test
+    public void updateRoomTest() throws ResourceNotFoundException {
 
-		// given - setup or precondition
-		Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
-		Room savedRoom = roomService.createRoom(room);
-		Room roomToUpdate = Room.builder().id(1L).name("Java 9").date("06/26/2022").startingTime("15h00").endingTime("20h00").build();
+        // given - setup or precondition
+        Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
+        Room savedRoom = roomService.createRoom(room);
+        Room roomToUpdate = Room.builder().id(1L).name("Java 9").date("06/26/2022").startingTime("15h00").endingTime("20h00").build();
 
-		// when - action or the testing
-		Room updatedRoom = roomService.updateRoom(room.getId(), roomToUpdate);
+        // when - action or the testing
+        Room updatedRoom = roomService.updateRoom(room.getId(), roomToUpdate);
 
-		// then - very output
-		assertEquals(updatedRoom.toString(), roomToUpdate.toString());
-	}
+        // then - very output
+        assertEquals(updatedRoom.toString(), roomToUpdate.toString());
+    }
 
-	@Test
-	public void deleteRoomTest() throws ResourceNotFoundException {
+    @Test
+    public void deleteRoomTest() throws ResourceNotFoundException {
 
-		// given - setup or precondition
-		Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
+        // given - setup or precondition
+        Room room = Room.builder().id(1L).name("Java 8").date("06/26/2022").startingTime("15h00").endingTime("18h00").build();
 
-		// when - action or the testing
-		var testResult = roomService.deleteRoom(room.getId());
+        // when - action or the testing
+        var testResult = roomService.deleteRoom(room.getId());
 
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("Deleted", Boolean.TRUE);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Deleted", Boolean.TRUE);
 
-		// then - very output
-		assertEquals(response, testResult);
-	}
+        // then - very output
+        assertEquals(response, testResult);
+    }
 
 }
